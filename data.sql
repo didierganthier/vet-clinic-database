e309c16f3d43b941ad0ca9967eff2df445e1f941
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-12-06 11:22:13 -0500

    Add species

--- data.sql
+++ data.sql
@@ -5,3 +5,5 @@ VALUES ('Agumon', DATE '2020-02-03', 0, true, 10.23),
          ('Gabumon', DATE '2018-11-15', 2, true, 8),
          ('Pikachu', DATE '2021-01-07', 1, false, 15.04),
          ('Devimon', DATE '2017-05-12', 5, true, 11);
+
+
